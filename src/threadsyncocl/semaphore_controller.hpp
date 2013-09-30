f056d88ae1c5c729279fbd2ff92e37b86f6f60cc
Chatsiri Rattana <chatsiri@chatsiri.com>
2013-09-30 23:11:25 +0700

    Semaphore class add to threadsynocl directory

--- src/threadsyncocl/semaphore_controller.hpp
+++ src/threadsyncocl/semaphore_controller.hpp
@@ -1,33 +1,34 @@
 #ifndef CONTROLLER__SEMAPHORE_CONTROLLER_HPP_
-#define CONTROLLER__SEMAPHORE_CONTROLLER_HPP_ 
+#define CONTROLLER__SEMAPHORE_CONTROLLER_HPP_
 
 #include <boost/interprocess/smart_ptr/unique_ptr.hpp>
 
 namespace controller
 {
 
-template<typename SemaphoreProcesses>
-class semaphore
-{
-	public:
-		virtual lock_request()   = 0;
-		virtual unlock_request() = 0;
-		virtual boost::unique_ptr<SemaphoreProcesses> processes() = 0;
-};
+    template<typename SemaphoreProcesses>
+    class semaphore
+    {
+        public:
+            virtual lock_request()   = 0;
+            virtual unlock_request() = 0;
+            virtual boost::unique_ptr<SemaphoreProcesses> processes() = 0;
+    };
 
-// Mutex 
-template<typename Mutex>
-class mutex_buffer
-{
-    public:
-				init();
-				lock_request();
-				try_lock();
-				unlock_request();
-				destruction();
-    private:
-				pthread_mutex_t mutex;
-};
+    // Mutex
+    template<typename Mutex>
+    class mutex_buffer : public semaphore<Mutex>
+    {
+        public:
+            init();
+            lock_request();
+            unlock_request();
+            try_lock();
+            boost::unique_ptr<Mutex> processes();
+            destruction();
+        private:
+            pthread_mutex_t mutex;
+    };
 
 }
 
